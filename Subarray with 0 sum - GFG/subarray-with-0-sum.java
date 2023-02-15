4b2e5cce44645eb9f2a7b8ba932392a46e924e79
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-02-15 22:38:23 +0530

    Added solution - LeetHub

---
+++ Subarray with 0 sum - GFG/subarray-with-0-sum.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+			//code
+			
+		//taking input using class Scanner
+		Scanner scan = new Scanner(System.in);
+		
+		//taking total number of testcases
+		int t = scan.nextInt();
+		while(t>0)
+		{
+		    //taking total count of elements
+		    int n = scan.nextInt() ;
+		    
+		    //Declaring and Initializing an array of size n
+		    int arr[] = new int[n];
+		    
+		    //adding elements to the array
+		    for(int i = 0; i<n;i++)
+		    {
+		        arr[i] = scan.nextInt();
+		    }
+		    
+		    t--;
+		    
+		     //calling the method findSum
+		     //and print "YES" or "NO" based on the result
+		     System.out.println(new Solution().findsum(arr,n)==true?"Yes":"No");
+		}
+	}
+	
+	
+}
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to check whether there is a subarray present with 0-sum or not.
+    static boolean findsum(int arr[],int n)
+    {
+        HashSet<Integer> p= new HashSet<>();
+        for(int i=0;i<n;i++){ 
+            if(arr[i]==0){return true; }
+        }
+        p.add(arr[0]);
+        
+        for (int i=1;i<arr.length;i++){
+         arr [i]=arr[i]+arr[i-1];  
+         p.add(arr[i]);
+        }
+        if(p.size()!=n || p.contains(0)){ 
+            return true;
+        }
+        return false;
+    
+    }
+}
