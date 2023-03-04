07e8d758ecdc918e5b5466ab1e93e7c2c8695cc0
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-03-04 20:07:19 +0530

    Added solution - LeetHub

---
+++ Peak element - GFG/peak-element.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+
+
+import java.util.*;
+import java.io.*;
+class PeakElement{
+	public static void main(String[] args)throws IOException{
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+		int t=Integer.parseInt(in.readLine().trim());
+		while(t-->0){
+			int n=Integer.parseInt(in.readLine().trim());
+			int[] a=new int[n];
+		    int[] tmp=new int[n];
+		    String s[]=in.readLine().trim().split(" ");
+			for(int i=0;i<n;i++){
+				a[i]=Integer.parseInt(s[i]);
+				tmp[i] = a[i];
+			}
+		    
+		    int f=0;
+			int A=(new Solution()).peakElement(tmp,n);
+			
+			if(A<0 && A>=n)
+			    out.println(0);
+			else
+			{
+    			if(n==1 && A==0)
+    			    f=1;
+    			else if(A==0 && a[0]>=a[1])
+    			    f=1;
+    			else if(A==n-1 && a[n-1]>=a[n-2])
+    			    f=1;
+    			else if(A >= 0 && A < n && a[A] >=a[A+1] && a[A]>= a[A-1])
+    			    f=1;
+    			else
+    			    f=0;
+			    out.println(f);
+			}
+		}
+		out.close();
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution
+{
+	// Function to find the peak element
+	// arr[]: input array
+	// n: size of array a[]
+	public int peakElement(int[] arr,int n)
+    { 
+        if(arr.length==1 ||arr[0]>=arr[1]){return 0;}
+         else if(arr[n-1]>=arr[n-2]){return n-1;}
+         
+     int start=0; int end=n-1;
+     while(start<end){ 
+        int mid=(start+end)/2;
+        if (arr[mid]>=Math.max(arr[mid-1],arr[mid+1])){return mid;}
+       else{ start=mid+1;}
+         
+     }
+    int val=0; 
+     start=0;  end=n-1;
+     while(start<end){ 
+        int mid=(start+end)/2;
+        if (arr[mid]>=Math.max(arr[mid-1],arr[mid+1])){
+           val=mid;
+           break;
+        }
+       else{ end=mid;}
+         
+     }
+        
+    return val;
+      
+    }
+}
