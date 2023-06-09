89defbd5e1892738d45eaba71ccae318ca281d2e
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-06-10 00:24:45 +0530

    Added solution - LeetHub

---
+++ Arranging the array - GFG/arranging-the-array.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.Rearrange(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public void Rearrange(int a[], int n)
+    {
+        Queue<Integer> q1= new LinkedList<>();
+        Queue<Integer> q2= new LinkedList<>(); 
+        
+        for(int i=0;i<n;i++){ 
+         
+         if(a[i]<0){ 
+             q1.add(a[i]);
+         }
+         else{ 
+          q2.add(a[i]);   
+         }
+            
+        }
+        
+        int i=0;
+        while(!q1.isEmpty()){ 
+            a[i++]=q1.remove();
+        }
+        
+        while(!q2.isEmpty()){ 
+            a[i++]=q2.remove();
+        }
+        
+    
+    
+        
+    }
+}
