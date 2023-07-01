6ab993fdd88ca8c0976503e5bdea44136bdbf59a
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-07-01 12:06:41 +0530

    Added solution - LeetHub

---
+++ Pair with given sum in a sorted array - GFG/pair-with-given-sum-in-a-sorted-array.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+            
+
+class Solution{
+    
+  
+    int Countpair(int a[], int n, int sum)
+    {
+        
+        int count=0;
+        int i=0,j=n-1;
+        while(i<j){ 
+         
+         if(a[i]+a[j]>sum){j--;}
+         else if(a[i]+a[j]<sum){i++;}
+         else{ 
+          i++;
+          j--;
+          count++;
+         }
+            
+        }
+        
+        return count!=0?count:-1;
+        
+    }
+  
+    
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    
+		    String line1 = br.readLine();
+		    String[] element = line1.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    int K = Integer.parseInt(br.readLine());
+		    
+		    Solution obj = new Solution();
+		   
+		    int res = obj.Countpair(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		    
+		    
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
