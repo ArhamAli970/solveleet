014df304019b8219881f9554425e4665c54078f5
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-07-17 00:46:00 +0530

    Added solution - LeetHub

--- First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.java
+++ First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.java
@@ -7,14 +7,16 @@ class GFG
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
         int T = Integer.parseInt(br.readLine().trim());
         while(T-->0)
         {
             String A = br.readLine().trim();
             Solution ob = new Solution();
             String ans = ob.FirstNonRepeating(A);
-            System.out.println(ans);
+            out.println(ans);
         }
+        out.close();
     }
 }
 
@@ -23,29 +25,35 @@ class GFG
 
 class Solution
 {
-    public String FirstNonRepeating(String A)
+    public String FirstNonRepeating(String s)
     {
+        int n=s.length();
         StringBuilder sb= new StringBuilder();
-      Queue <Character> q= new LinkedList<>();
-      int freq[]= new int[26];
-      
-      for (int i=0;i<A.length();i++){ 
-       
-       freq[A.charAt(i)-97]++;
-       q.add(A.charAt(i));
-       
-       while(freq[q.peek()-97]!=1){
-           q.remove(); 
-           if(q.isEmpty()){
-               sb.append("#"); 
-               break;
-           }
-       } 
-       if(!q.isEmpty()){sb.append(q.peek());}
-       
-          
-      }
-      return sb.toString();
-      
+        int arr[]= new int[26];
+        Queue<Character> q= new LinkedList<>();
+        
+        for(int i=0;i<n;i++){ 
+            ++arr[s.charAt(i)-'a'];
+            q.add(s.charAt(i));
+            
+            while(!q.isEmpty() && arr[q.peek()-'a']>1){ 
+             q.remove();   
+            }
+            
+            if(q.isEmpty()){
+            sb.append("#");
+            }
+            else{
+             sb.append(q.peek());
+             
+            }
+                
+            
+            
+            
+        }
+        
+        return sb.toString();
+        
     }
 }
