9dea5116029b30cad837671a9b58022cad16bafd
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-10-13 00:23:37 +0530

    Added solution - LeetHub

---
+++ Floor in BST - GFG/floor-in-bst.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class Node {
+    int data;
+    Node right;
+    Node left;
+
+    Node(int val) {
+        data = val;
+        right = null;
+        left = null;
+    }
+}
+
+class GFG {
+
+    public static Node insert(Node tree, int val) {
+        Node temp = null;
+        if (tree == null) {
+            return new Node(val);
+        }
+
+        if (val < tree.data) {
+            tree.left = insert(tree.left, val);
+        } else if (val > tree.data) {
+            tree.right = insert(tree.right, val);
+        }
+
+        return tree;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            Node root = null;
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                int k = sc.nextInt();
+                root = insert(root, k);
+            }
+
+            int s = sc.nextInt();
+
+            Solution obj = new Solution();
+            int ans = obj.floor(root, s);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int max;
+    public static void call(Node root,int x){ 
+     
+     if(root==null){return;}
+     
+     if(root.data<=x && root.data>max){ 
+         max=root.data;
+     }
+     
+     call(root.left,x);
+     call(root.right,x);
+        
+    }
+    
+    public static int floor(Node root, int x) {
+    max=-1;
+    call(root,x);
+    
+    return max;
+    
+    
+    }
+}
