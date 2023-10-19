d6e1002a403eef830c48b6419bf8f1c6ac671b65
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2023-10-19 19:37:50 +0530

    Added solution - LeetHub

---
+++ Level of Nodes - GFG/level-of-nodes.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for(int i = 0; i < V+1; i++)
+                list.add(i, new ArrayList<Integer>());
+            for(int i = 0; i < E; i++)
+            {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+                list.get(v).add(u);
+            }
+            int X = sc.nextInt();
+            
+            Solution ob = new Solution();
+            
+            System.out.println(ob.nodeLevel(V,list,X));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution
+{
+    //Function to find the level of node X.
+    int nodeLevel(int V, ArrayList<ArrayList<Integer>> adj, int X)
+    {
+        int cnt=0;
+        
+        Queue<Integer> q= new LinkedList<>();
+        HashSet<Integer> h= new HashSet<>();
+        
+        q.add(0);
+        
+        while(!q.isEmpty()){ 
+         
+         int size=q.size();
+         for(int i=0;i<size;i++){ 
+             
+             int curr=q.remove();
+             h.add(curr);
+             
+             for(int j=0;j<adj.get(curr).size();j++){
+                 
+                 int val=adj.get(curr).get(j);
+                 if(val==X){return cnt+1;}
+                 if(!h.contains(val)){ 
+                  q.add(val);   
+                 }
+                 
+             }
+             
+             
+         }
+            cnt++;
+            
+        }
+        
+        return -1;
+        
+    }
+}
